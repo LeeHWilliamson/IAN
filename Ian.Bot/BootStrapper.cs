f410b508f32c1689139852fe1385ddc5872b72a5
theAdmiral21 <thomas.lee.williamson@gmail.com>
2026-08-08 13:38:13 -0500

    feat: adds a bot bootstrapper
    Adds an entry point/bootstrapper for starting the bot.

---
+++ Ian.Bot/BootStrapper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using NetCord.Hosting.Gateway;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Register services
+builder.Services
+    .AddDiscordGateway();
+
+builder.Configuration.AddUserSecrets<Program>();
+
+// Build everything
+var host = builder.Build();
+
+await host.RunAsync();
